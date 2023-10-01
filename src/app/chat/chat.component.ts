34f2de90ed1de9fe5e01330da714d565d85e1d9c
Hasitha Perera <hasithaperera2017@.com>
2023-10-01 20:36:02 +0530

    commit 2

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,8 +1,15 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+import {ActivatedRoute, Route, Router} from "@angular/router";
+
+
 
 interface ChatMessage {
   senderName: string;
+  receiverName: string;
   message: string;
+  status: string;
 }
 
 @Component({
@@ -10,27 +17,103 @@ interface ChatMessage {
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit{
   userName!: string;
   chatName!: string;
 
+  private stompClient: any;
   privateChats: Map<string, ChatMessage[]> = new Map<string, ChatMessage[]>();
   publicChats: ChatMessage[] = [];
 
   @ViewChild('messageInput') messageInput: any;
-  constructor() {
-    this.chatName = 'Chat';
-    this.userName = 'User';
+
+  constructor(private route: Router,private router:ActivatedRoute) {
+    this.chatName = 'ChatRoom';
+    this.userName = this.router.snapshot.queryParams['name'];
+    this.route = route;
   }
 
 
-
-  sendMessage(value: string) {
-    console.log(value)
-    this.messageInput.nativeElement.value = '';
-  }
 
   setChatName(chatRoom: string) {
     this.chatName = chatRoom;
+  }
+
+  onRegister() {
+    let sockJs = new SockJS('http://localhost:8080/ws');
+    this.stompClient = Stomp.over(sockJs);
+    this.stompClient.connect({}, this.onConnected, this.onErrorMessage);
+  }
+
+  onErrorMessage = () => {
+    this.route.navigate(['/login'])
+  };
+
+
+
+  onPublicMessage = (payload: any) => {
+    let publicMessage: ChatMessage = JSON.parse(payload.body);
+    switch (publicMessage.status) {
+      case 'JOIN':
+        console.log(publicMessage.senderName + ' has joined the chat room');
+        if (!this.privateChats.has( publicMessage.senderName) && publicMessage.senderName !== this.userName) {
+          this.privateChats.set(publicMessage.senderName, []);
+        }
+        break;
+      case 'LEAVE':
+        console.log(publicMessage.senderName + ' has left the chat room');
+        break;
+      case 'CHAT':
+        if (publicMessage.senderName !== this.userName) {
+          this.publicChats.push(publicMessage);
+        }
+        break;
+
+    }
+  };
+  onPrivateMessage = (payload:any ) => {
+    let privateMessage: ChatMessage = JSON.parse(payload.body);
+    if (this.privateChats.has(privateMessage.senderName)) {
+      this.privateChats.get(privateMessage.senderName)?.push(privateMessage);
+    } else {
+      this.privateChats.set(privateMessage.senderName, [privateMessage]);
+    }
+  };
+  onConnected = () => {
+    this.stompClient.subscribe('/topic/public', this.onPublicMessage);
+    this.stompClient.subscribe('/user/' + this.userName + '/private', this.onPrivateMessage);
+    let joinMessage: ChatMessage = {
+      senderName: this.userName,
+      receiverName: 'ChatRoom',
+      message: this.userName + ' has joined the chat room',
+      status: 'JOIN'
+    }
+    this.stompClient.send('/app/public', {}, JSON.stringify(joinMessage));
+  };
+  onSendMessage = (message: string) => {
+    if (this.chatName === 'ChatRoom') {
+      let sendingMessage :ChatMessage = {
+        senderName: this.userName,
+        receiverName: this.chatName,
+        message: message,
+        status: 'CHAT'
+      }
+      this.stompClient.send('/app/public', {}, JSON.stringify(sendingMessage),this.onErrorMessage);
+      this.publicChats.push(sendingMessage);
+    } else {
+      let sendingMessage :ChatMessage = {
+        senderName: this.userName,
+        receiverName: this.chatName,
+        message: message,
+        status: 'CHAT'
+      }
+      this.stompClient.send('/app/private', {}, JSON.stringify(sendingMessage));
+      this.privateChats.get(this.chatName)?.push(sendingMessage);
+  }
+    this.messageInput.nativeElement.value = '';
+  }
+
+  ngOnInit(): void {
+    this.onRegister()
   }
 }
